9dfa24fd13655205db77b785bdc4072d6490b369
IgorMontezuma20 <igorminranda17@gmail.com>
2025-03-01 12:00:32 -0300

    build: Add Clerk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
-import { Button } from "@/components/ui/button";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-      <Button>Login</Button>
+    <div className="m-10">
+      <header className="flex justify-end items-center p-4 gap-4 h-16">
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </header>
     </div>
   );
 }
