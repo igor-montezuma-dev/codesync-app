cc179cb0b331c795e1aa6b0e65af060c6c091589
IgorMontezuma20 <igorminranda17@gmail.com>
2025-03-01 12:40:56 -0300

    build: Add convex and build user schema

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,3 @@
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
-
 export default function Home() {
-  return (
-    <div className="m-10">
-      <header className="flex justify-end items-center p-4 gap-4 h-16">
-        <SignedOut>
-          <SignInButton />
-        </SignedOut>
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
-      </header>
-    </div>
-  );
+  return <div className="m-10">Home page</div>;
 }
