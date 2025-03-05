eebb196b6c77acec8ffe8bc6ed2f7b398cb23032
IgorMontezuma20 <igorminranda17@gmail.com>
2025-03-05 12:55:57 -0300

    feat: Create candidate view ui

--- src/app/(root)/(home)/page.tsx
+++ src/app/(root)/(home)/page.tsx
@@ -6,7 +6,9 @@ import { useUserRole } from "@/hooks/useUserRole";
 import { useQuery } from "convex/react";
 
 import LoaderUI from "@/components/LoaderUI";
+import MeetingCard from "@/components/MeetingCard";
 import MeetingModal from "@/components/MeetingModal";
+import { CalendarOffIcon, Loader2Icon } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { api } from "../../../../convex/_generated/api";
@@ -75,7 +77,37 @@ export default function Home() {
         </>
       ) : (
         <>
-          <div>Painel do candidato é aqui</div>
+          <div>
+            <h1 className="text-3xl font-bold">Suas entrevistas</h1>
+            <p className="text-muted-foreground mt-1">
+              Veja e junte-se às suas entrevistas agendadas
+            </p>
+          </div>
+
+          <div className="mt-8">
+            {interviews === undefined ? (
+              <div className="flex justify-center py-12">
+                <Loader2Icon className="size-8 animate-spin text-muted-foreground" />
+              </div>
+            ) : interviews.length > 0 ? (
+              <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+                {interviews.map((interview) => (
+                  <MeetingCard
+                    key={interview._id}
+                    interview={interview}
+                  ></MeetingCard>
+                ))}
+              </div>
+            ) : (
+              <div className="flex justify-center py-12">
+                <p className="text-center py-12 text-muted-foreground">
+                  Sem entrevistas agendadas
+                </p>
+
+                <CalendarOffIcon className="size-8 text-emerald-500 animate-pulse" />
+              </div>
+            )}
+          </div>
         </>
       )}
     </div>
